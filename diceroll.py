98e76420e88570922e89f433cba387e19448b3e2
Wyatt Deane <wyattocta@gmail.comm>
2025-11-05 09:31:35 -0500

    Initial commit

---
+++ diceroll.py
@@ -0,0 +1,21 @@
+import random
+
+def diceRoll():
+    diceValue = random.randint(1,6)
+    return diceValue
+
+def startGame():
+    print('Welcome to Dice Roll! its your roll vs theirs')
+    ourRoll = diceRoll()
+    theirRoll = diceRoll()
+    print("You rolled a " + str(ourRoll))
+    print("House rolled a " + str(theirRoll))
+    if ourRoll == theirRoll:
+        print('Draw')
+    elif ourRoll > theirRoll:
+        print('You win')
+    else:
+        print('You lose')
+
+
+startGame()
